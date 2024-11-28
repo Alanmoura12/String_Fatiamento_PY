6925d4101e8bc0ffc4f8a802c49a256192b66c3b
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-28 17:58:43 -0300

    Tipos de formatação de Strings

---
+++ string_3formatacao.py
@@ -0,0 +1,24 @@
+nome = "Antonio Ferras"
+
+#deixa a formatação sem precisar usar a concatenação ou outros tipos
+mensagem = f'''   
+  Olá meu nome é {nome},
+Eu estou aprendendo Python.
+        Essa mensagem tem diferentes recuos.
+'''
+
+print(mensagem)
+
+
+print("""
+  ===========  MENU  =============
+      
+  1 - Depositar
+  2 - Sacar
+  3 - Sair
+
+   ===============================
+      
+    Obrigadoo por usar nosso sistema!!!
+"""
+)
